8af188813d06660f73dcdb975357eef23b1fef45
Przemekhash <56091015+Przemekhasz@users.noreply.github.com>
2021-09-04 15:37:35 +0200

    Repair method findByUserName -> findByUername

--- src/main/java/pl/ptarapacki/userservice/repo/UserRepo.java
+++ src/main/java/pl/ptarapacki/userservice/repo/UserRepo.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pl.ptarapacki.userservice.domain.User;
 
 public interface UserRepo extends JpaRepository<User, Long> {
-    User findByUserName(String username);
+    User findByUsername(String username);
 }
